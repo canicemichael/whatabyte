c5d277f6e8753dd98943e35f40fdf89be52a227b
canicemichael <canicemichael@gmail.com>
2022-02-14 13:30:05 +0100

    Update index.js package.json

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ const port = process.env.PORT || "8001";
 /**
  * App Configuration
  */
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 /**
  * Routes Definitions
  */
 app.get('/', (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
-})
+    res.render("index", { title: "Home" });
+});
 
 /**
  * Server Activation
